0dd0c382d9cd3ada2a4f7ef368f24750e91b11c1
md <texet0598@gmail.com>
2025-11-19 23:22:16 +0300

    test: add test for model email

--- src/models/email.rs
+++ src/models/email.rs
@@ -1,8 +1,9 @@
 use serde::{Deserialize, Serialize};
+use serde_email::Email;
 
 #[derive(Debug, Deserialize)]
 pub struct EmailRequest {
-    pub email: String,
+    pub email: Email,
     pub code: String,
 }
 
@@ -10,4 +11,20 @@ pub struct EmailRequest {
 pub struct EmailResponse {
     pub success: bool,
     pub message: String,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn valid_email() {
+        let email = Email::from_str("echor.support@gmail.com").unwrap();
+        let test_email_request = EmailRequest {
+            email,
+            code: String::from("4483"),
+        };
+
+        assert_eq!(test_email_request.email.as_str(), "echor.support@gmail.com");
+    }
 }
